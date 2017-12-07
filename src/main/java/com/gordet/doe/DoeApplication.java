aee34a16d8a040e1ec8f715bf1a1bd94dcc49081
Bengo <benoit.gordet@gmail.com>
2017-12-07 07:37:24 +0100

    first commit

---
+++ src/main/java/com/gordet/doe/DoeApplication.java
@@ -0,0 +1,12 @@
+package com.gordet.doe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoeApplication.class, args);
+	}
+}
